7d55d204d857943b0940b87e00abdf773025c659
Seyeon Lim <sammys27@naver.com>
2024-11-19 00:29:43 +0900

    add frontservice

---
+++ frontservice/src/main/java/com/caboooom/frontservice/IndexController.java
@@ -0,0 +1,24 @@
+package com.caboooom.frontservice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signupForm";
+    }
+
+}
---
+++ frontservice/src/test/java/com/caboooom/frontservice/FronttestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caboooom.frontservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FronttestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
